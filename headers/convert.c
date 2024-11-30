66b05da6cecde9509c2373da91e634958d40c75c
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2024-12-01 00:44:03 +0100

    Created function to compute power as uint8_t

--- headers/convert.c
+++ headers/convert.c
@@ -1,5 +1,15 @@
 #include "convert.h"
 
+uint8_t pow(uint8_t a, uint8_t b) {
+	uint8_t res = 1;
+
+	for (uint8_t i=0; i<b; i++) {
+		res *= a;
+	}
+
+	return res;
+}
+
 uint32_t strtoIntIP(char* ip_str) {
 	IPv4_t ip = 0;
 	uint8_t octet = 0;
